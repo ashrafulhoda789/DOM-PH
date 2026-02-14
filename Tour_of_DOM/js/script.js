11306deb1887d6dad9a20a9e35d58eb799ba2282
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-14 20:12:22 +0600

    create html in Js

--- Tour_of_DOM/js/script.js
+++ Tour_of_DOM/js/script.js
@@ -31,3 +31,52 @@ const playerList = document.getElementById('player-list');
 
 // append the child to the parent
 playerList.appendChild(newChild)
+
+
+// Append 
+/**
+ * Parent node
+*/
+
+// 1. parent node
+const  mainContainer = document.getElementById('main-container');
+// console.log(mainContainer);
+
+//2. Create child node
+const placesSection = document.createElement('section');
+
+// create h1
+const h1 = document.createElement('h1');
+h1.innerText = 'Places I want to visit';
+placesSection.appendChild(h1);
+
+const ul = document.createElement('ul');
+
+const li1 = document.createElement('li');
+li1.innerText = 'Bandorban';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = "Cox's Bazar";
+ul.appendChild(li2);
+
+placesSection.appendChild(ul);
+
+// 3. append placesSection to the mainContainer
+mainContainer.appendChild(placesSection);
+
+
+
+// Easier to create HTML
+const booksSection = document.createElement('section');
+
+booksSection.innerHTML = `
+    <h1>Books I need to read</h1>
+    <ul>
+        <li>Physics</li>
+        <li>Chemistry</li>
+        <li>Higher Math</li>
+    </ul>
+`;
+
+mainContainer.appendChild(booksSection);
